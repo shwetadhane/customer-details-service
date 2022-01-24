25c017638acdb314c4d07980bd41741df70bf141
Admin <shwtjadhav@gmail.com>
2022-01-24 00:50:53 +0000

    calling to banking detail service with hardcode url

--- src/main/java/com/cg/customerdetailsservice/CustomerDetailsServiceApplication.java
+++ src/main/java/com/cg/customerdetailsservice/CustomerDetailsServiceApplication.java
@@ -2,9 +2,16 @@ package com.cg.customerdetailsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class CustomerDetailsServiceApplication {
+
+	@LoadBalanced
+	public WebClient.Builder getWebClient(){
+		return WebClient.builder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerDetailsServiceApplication.class, args);
--- src/main/java/com/cg/customerdetailsservice/controller/CustomerDetailsController.java
+++ src/main/java/com/cg/customerdetailsservice/controller/CustomerDetailsController.java
@@ -1,16 +1,34 @@
 package com.cg.customerdetailsservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 @EnableDiscoveryClient
 public class CustomerDetailsController {
 
+    @Autowired
+    WebClient.Builder webClientBuilder;
+
+    @Autowired
+    DiscoveryClient discoveryClient;
+
     @RequestMapping("/details")
     public String getCustomerDetails(){
-        return "Shweta";
+
+
+        String bankDetail = webClientBuilder.build()
+                .get()
+                .uri("http://172.22.41.201:31229/bankdetails")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        return "Shweta : "+ bankDetail + " ********Services : "+discoveryClient.getServices();
     }
 
 }
